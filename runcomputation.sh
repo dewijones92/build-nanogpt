1ab13d4f2406d835a7e0f7060f85992b9b1defc3
Dewi Jones <dewijones92@gmail.com>
2024-07-20 08:58:08 +0100

    more

--- runcomputation.sh
+++ runcomputation.sh
@@ -1,3 +1,1 @@
-{ bash spec.sh; python train_gpt2.py; } |& tee run_logs/$(date +%s%3N)_$(date +%Y-%m-%d_%H-%M-%S).goodlog;
-
-sshpass -p 'testpass' rsync -avvvz --exclude=".*" --include="*.goodlog" ./run_logs  wb1_user@dewijones92vultr.duckdns.org:/home/wb1_user/code/build-nanogpt;
+{ bash spec.sh; python train_gpt2.py; } | ssh wb1_user@dewijones92vultr.duckdns.org "cat > /home/wb1_user/code/build-nanogpt/run_logs/$(date +%s%3N)_$(date +%Y-%m-%d_%H-%M-%S).goodlog"
