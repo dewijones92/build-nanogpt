953d3e748c91dff4ac4fdd9909abe79e952bb106
Dewi Jones <dewijones92@gmail.com>
2024-07-20 09:43:44 +0100

    more

--- train_gpt2.py
+++ train_gpt2.py
@@ -510,7 +510,7 @@ for step in range(max_steps):
                 f.write(f"{step} hella {acc_norm:.4f}\n")
 
     # once in a while generate from the model (except step 0, which is noise)
-    if ((step > 0 and step % 250 == 0) or last_step) and (not use_compile):
+    if ((step > -1 and step % 250 == 0) or last_step) and (not use_compile):
         model.eval()
         num_return_sequences = 4
         max_length = 32
