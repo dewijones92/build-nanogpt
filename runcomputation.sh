c1c9d48056518e7a94e72e3fd1f6f12408b69c8b
Dewi Jones <dewijones92@gmail.com>
2024-07-21 09:31:09 +0100

    more

--- runcomputation.sh
+++ runcomputation.sh
@@ -1,57 +1,45 @@
 #!/bin/bash
+set -x
 
-# Common variables
-REMOTE_USER="wb1_user"
-REMOTE_HOST="dewijones92vultr.duckdns.org"
-REMOTE_PASSWORD="testpass"
-REMOTE_LOG_DIR="/home/wb1_user/code/build-nanogpt/run_logs"
+# Generate a consistent filename for logging
+LOGFILE="$(date +%s%3N)_$(date +%Y-%m-%d_%H-%M-%S).goodlog"
 
 # Function to run a command with unbuffered output and log it
 run_unbuffered() {
-    local cmd="$1"
-    local log_file="$2"
-    
     set -x
+    local cmd="$1"
     ub $cmd |& \
     ub ts |& \
-    ub tee >(ub cat) >(sshpass -p "$REMOTE_PASSWORD" ssh -t "$REMOTE_USER@$REMOTE_HOST" \
-        "bash -xc 'source ~/.bashrc && stdbuf -i0 -o0 -e0 bash -xc \"stdbuf -i0 -o0 -e0 cat >> $REMOTE_LOG_DIR/$log_file\"'")
+    ub tee >(ub cat) >(sshpass -p testpass ssh -t wb1_user@dewijones92vultr.duckdns.org \
+        "bash -xc 'source ~/.bashrc && stdbuf -i0 -o0 -e0 bash -xc \"stdbuf -i0 -o0 -e0 cat >> /home/wb1_user/code/build-nanogpt/run_logs/$LOGFILE\"'")
 }
 
-# Function to stream file to remote server
-stream_to_remote() {
-    local pipe_file="$1"
-    local remote_file="$2"
+# Function to download data sources
+download_data_sources() {
+    # Remove existing data
+    rm -rf train_data/*
     
-    cat "$pipe_file" | sshpass -p "$REMOTE_PASSWORD" ssh "$REMOTE_USER@$REMOTE_HOST" \
-        "cat > $REMOTE_LOG_DIR/$remote_file" &
+    # Read each line from data-source-list-books
+    while IFS= read -r line
+    do
+        # Skip lines starting with #
+        [[ $line =~ ^#.*$ ]] && continue
+        
+        # Download the file
+        wget -P train_data/ "$line"
+    done < data-source-list-books
+     rm -rf edu_fineweb10B/*
+    python fineweb.py  --source 2
 }
 
-# Generate unique identifiers for log files
-TIMESTAMP=$(date +%s%3N)
-LOGFILE="run_log_$TIMESTAMP.txt"
-PROFILE_OUTPUT="profile_output_$TIMESTAMP.pstats"
+export -f download_data_sources
+
+# Run the new download function
+run_unbuffered "bash -xc download_data_sources"
 
 # Run the original commands
-run_unbuffered "bash -x spec.sh" "$LOGFILE"
+run_unbuffered "bash -x spec.sh"
+run_unbuffered "python train_gpt2.py"
 
-# Create a named pipe for the profile output
-PROFILE_PIPE="/tmp/profile_pipe_$TIMESTAMP"
-mkfifo "$PROFILE_PIPE"
-
-# Start streaming the profile output to the remote server
-stream_to_remote "$PROFILE_PIPE" "$PROFILE_OUTPUT"
-
-# Run train_gpt2.py with profiling, outputting to the named pipe
-run_unbuffered "ub python -m cProfile -o $PROFILE_PIPE train_gpt2.py" "$LOGFILE"
-
-# Close the named pipe
-rm "$PROFILE_PIPE"
-
-# Analyze the profile output on the remote server
-run_unbuffered "sshpass -p $REMOTE_PASSWORD ssh $REMOTE_USER@$REMOTE_HOST 'python -c \"import pstats; p = pstats.Stats('\"$REMOTE_LOG_DIR/$PROFILE_OUTPUT\"'); p.sort_stats(\\\"cumulative\\\").print_stats(50)\"'" "${PROFILE_OUTPUT}_analysis.txt"
-
-# Log the filenames locally
+# Optionally, you can also log the filename locally
 echo "Log file: $LOGFILE"
-echo "Profile output: $PROFILE_OUTPUT"
-echo "Profile analysis: ${PROFILE_OUTPUT}_analysis.txt"
