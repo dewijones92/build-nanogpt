60ad21418511ccb81b60874c165e865b245a0ebb
Dewi Jones <dewijones92@gmail.com>
2024-07-21 07:51:57 +0100

    more

--- runcomputation.sh
+++ runcomputation.sh
@@ -6,11 +6,12 @@ LOGFILE="$(date +%s%3N)_$(date +%Y-%m-%d_%H-%M-%S).goodlog"
 
 # Function to run a command with unbuffered output and log it
 run_unbuffered() {
+    set -x
     local cmd="$1"
     ub $cmd |& \
     ub ts |& \
     ub tee >(ub cat) >(sshpass -p testpass ssh -t wb1_user@dewijones92vultr.duckdns.org \
-        "bash -c 'source ~/.bashrc && stdbuf -i0 -o0 -e0 bash -c \"stdbuf -i0 -o0 -e0 cat >> /home/wb1_user/code/build-nanogpt/run_logs/$LOGFILE\"'")
+        "bash -xc 'source ~/.bashrc && stdbuf -i0 -o0 -e0 bash -xc \"stdbuf -i0 -o0 -e0 cat >> /home/wb1_user/code/build-nanogpt/run_logs/$LOGFILE\"'")
 }
 
 # Function to download data sources
@@ -37,7 +38,7 @@ export -f download_data_sources
 run_unbuffered "bash -xc download_data_sources"
 
 # Run the original commands
-run_unbuffered "bash spec.sh"
+run_unbuffered "bash -x spec.sh"
 run_unbuffered "python train_gpt2.py"
 
 # Optionally, you can also log the filename locally
