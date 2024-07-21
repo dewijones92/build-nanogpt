ddfe8ef6e8e332607abba6be3d33135866f634c5
Dewi Jones <dewijones92@gmail.com>
2024-07-21 09:17:14 +0100

    more

--- runcomputation.sh
+++ runcomputation.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
-set -x
 
-# Generate a consistent filename for logging
-LOGFILE="$(date +%s%3N)_$(date +%Y-%m-%d_%H-%M-%S).goodlog"
+# Common variables
+REMOTE_USER="wb1_user"
+REMOTE_HOST="dewijones92vultr.duckdns.org"
+REMOTE_PASSWORD="testpass"
+REMOTE_LOG_DIR="/home/wb1_user/code/build-nanogpt/run_logs"
 
 # Function to run a command with unbuffered output and log it
 run_unbuffered() {
-    set -x
     local cmd="$1"
+    local log_file="$2"
+    
+    set -x
     ub $cmd |& \
     ub ts |& \
-    ub tee >(ub cat) >(sshpass -p testpass ssh -t wb1_user@dewijones92vultr.duckdns.org \
-        "bash -xc 'source ~/.bashrc && stdbuf -i0 -o0 -e0 bash -xc \"stdbuf -i0 -o0 -e0 cat >> /home/wb1_user/code/build-nanogpt/run_logs/$LOGFILE\"'")
+    ub tee >(ub cat) >(sshpass -p "$REMOTE_PASSWORD" ssh -t "$REMOTE_USER@$REMOTE_HOST" \
+        "bash -xc 'source ~/.bashrc && stdbuf -i0 -o0 -e0 bash -xc \"stdbuf -i0 -o0 -e0 cat >> $REMOTE_LOG_DIR/$log_file\"'")
 }
 
-# Function to download data sources
-download_data_sources() {
-    # Remove existing data
-    rm -rf train_data/*
-    
-    # Read each line from data-source-list-books
-    while IFS= read -r line
-    do
-        # Skip lines starting with #
-        [[ $line =~ ^#.*$ ]] && continue
-        
-        # Download the file
-        wget -P train_data/ "$line"
-    done < data-source-list-books
-     rm -rf edu_fineweb10B/*
-    python fineweb.py  --source 2
+# Function to transfer file to remote server
+transfer_to_remote() {
+    local local_file="$1"
+    sshpass -p "$REMOTE_PASSWORD" scp "$local_file" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_LOG_DIR/"
 }
 
-export -f download_data_sources
-
-# Run the new download function
-run_unbuffered "bash -xc download_data_sources"
+# Generate unique identifiers for log files
+TIMESTAMP=$(date +%s%3N)
+LOGFILE="run_log_$TIMESTAMP.txt"
+PROFILE_OUTPUT="profile_output_$TIMESTAMP.pstats"
 
 # Run the original commands
-run_unbuffered "bash -x spec.sh"
-run_unbuffered "python train_gpt2.py"
+run_unbuffered "bash -x spec.sh" "$LOGFILE"
 
-# Optionally, you can also log the filename locally
+# Run train_gpt2.py with profiling
+run_unbuffered "ub python -m cProfile -o $PROFILE_OUTPUT train_gpt2.py" "$LOGFILE"
+
+# Analyze and stream the profile output
+run_unbuffered "ub python -c 'import pstats; p = pstats.Stats(\"$PROFILE_OUTPUT\"); p.sort_stats(\"cumulative\").print_stats(50)'" "$PROFILE_OUTPUT"
+
+# Transfer the .pstats file to the remote server
+transfer_to_remote "$PROFILE_OUTPUT"
+
+# Log the filenames locally
 echo "Log file: $LOGFILE"
+echo "Profile output: $PROFILE_OUTPUT"
