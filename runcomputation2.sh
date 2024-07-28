77f6e2ada928ad18084ad7d51ac896876a6d5b99
Your Name <you@example.com>
2024-07-28 06:22:01 +0100

    more

--- runcomputation2.sh
+++ runcomputation2.sh
@@ -1,4 +1,9 @@
 set -x
+
+
+docker run hello-world
+
+
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
   sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
