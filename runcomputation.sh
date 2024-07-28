3268a0a499c7d4314a79704c3599b81b15695361
Your Name <you@example.com>
2024-07-28 11:05:40 +0100

    more

--- runcomputation.sh
+++ runcomputation.sh
@@ -114,7 +114,7 @@ main_script() {
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
     sudo dpkg -i cuda-keyring_1.1-1_all.deb
     sudo apt-get update
-    sudo apt install nvidia-cuda-toolkit  
+    sudo apt-get -y install nvidia-cuda-toolkit  
     sudo apt-get -y install libcudnn9-dev-cuda-12
 
     sudo apt install -y openmpi-bin openmpi-doc libopenmpi-dev
